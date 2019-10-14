f16760ec9b1d39df8bf11389703911d86cc8d33a
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-14 00:52:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,6 +42,6 @@ def retrieve_last_element_from_array(array)
   array[5]
 end
 
-def update_element_from_index(array, index_number, element
+def update_element_from_index(array, index_number, element)
   array[4] = "totally"
 end
