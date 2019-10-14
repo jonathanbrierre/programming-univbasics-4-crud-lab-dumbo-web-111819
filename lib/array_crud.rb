ecf2dfc141a025fa0e4419272eb40feb919e0511
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-14 00:36:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puts "#{array}.pop("arrays!")"
-  puts "#{element}.pop("arrays")"
+  p "#{array}.pop("arrays!")"
+  p "#{element}.pop("arrays")"
 end
 
 def add_element_to_start_of_array(array, element)
